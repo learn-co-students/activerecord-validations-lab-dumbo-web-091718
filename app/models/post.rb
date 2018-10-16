a3a12c56c221c507fd55fa3a6d62c8c714fff99c
dianajyoo <diana.yooj@gmail.com>
2018-10-16 19:03:52 -0400

    finished

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait
+
+  def clickbait
+    # binding.pry
+    if title == nil
+      errors.add(:title, 'title cannot be nil!')
+    elsif !["Won't Believe", "Secret", "Top [number]", "Guess"].any? { |substring| title.include?(substring) }
+      errors.add(:title, 'needs to be clickbait')
+    end
+  end
+
 end
