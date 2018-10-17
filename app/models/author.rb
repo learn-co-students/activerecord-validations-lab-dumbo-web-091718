85973ca79889024b5c0f82f2cdf9be123118443d
brooklyndadcore <scott.waring@gmail.com>
2018-10-17 09:37:15 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :phone_number, format: { with: /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/,
+                              message: I18n.t('global.errors.phone_format')}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  CAT_OPTIONS = ["Fiction", "Non-Fiction"]
+  TITLE_OPTIONS = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+  validates :title, presence: true, uniqueness: true
+  validates_length_of :summary, :maximum => 250
+  validates_length_of :content, :minimum => 250
+  validates :category, presence: true, inclusion: CAT_OPTIONS
+  validate  :is_clickbait?
+
+  def is_clickbait?
+    TITLE_OPTIONS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
