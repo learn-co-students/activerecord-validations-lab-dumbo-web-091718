3dc1d36f6311ffe393dcfe0c0098a5925b251849
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-10-18 09:55:13 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,32 @@
-class Post < ActiveRecord::Base
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    # binding.pry
+    if record['title'] != nil
+      unless record['title'].include? "Won't Believe"
+        record.errors[:name] << 'Need title that includes "Won\'t Believe!"'
+      end
+    end
+
+  end
 end
+
+
+class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates_with MyValidator
+end
+
+
+
+
+# class ClickbaityValidator < ActiveModel::Validator
+#   def validate(title)
+#     unless record.name.starts_with? 'X'
+#       record.errors[:name] << 'Need a name starting with X please!'
+#     end
+#   end
+# end
